ef17c5294b9ec90728029bb3a410e691a4c65c3a
tyler-gilbert <tgil@coactionos.com>
2013-12-18 00:15:23 -0700

    state diagram generation (partial)

--- CoActionOS-Applib/include/State.hpp
+++ CoActionOS-Applib/include/State.hpp
@@ -3,6 +3,7 @@
 #ifndef STATE_HPP_
 #define STATE_HPP_
 
+
 /*! \brief State class (part of a state machine -- SMach) */
 class State {
 public:
@@ -38,6 +39,29 @@ public:
 
 	/*! \brief Checks to see if it is time to exit the state (optionally executes an exit action)*/
 	virtual State * exit_condition(State * list[]){ return 0; }
+
+	/*! \brief Called when exiting to the state specified by \a name */
+	/*! \details This is called when the state is about to exit.
+	 *
+	 * @param name Name of target state
+	 * @return \a true if it is OK to proceed with transition and \a false to abort transition
+	 */
+	virtual bool transition(const char * name){ return true; }
+
+	/*! \brief Exit condition list entry */
+	/*! \details This details the structure of an exit condition.
+	 *
+	 */
+	typedef struct {
+		const char * state /*! \brief The next state (if condition is true) */;
+		const char * desc /*! \brief A description of the condition (e.g. button press) */;
+		bool (*condition)(void) /*! \brief a function to execute to see if the condition is true */;
+	} transition_t;
+
+	virtual const transition_t * transition_list(void) const { return 0; }
+	virtual int transition_size(void) const { return 0; }
+
+	static bool istrue(){ return true; }
 
 };
 
